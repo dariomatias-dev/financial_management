70fd1ce929212f6bf052654c6d4c676134cde95a
dariomatias-dev <matiasdario75@gmail.com>
2024-12-31 11:01:42 -0300

    Criado classe ExpenseModel

---
+++ src/main/java/org/cafe/models/expense/ExpenseModel.java
@@ -0,0 +1,52 @@
+package org.cafe.models.expense;
+
+public class ExpenseModel {
+  private String name;
+  private double value;
+  private String period;
+  private String description;
+
+  public ExpenseModel(
+    String name,
+    double value,
+    String period,
+    String description
+  ) {
+    this.name = name;
+    this.value = value;
+    this.period = period;
+    this.description = description;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public double getValue() {
+    return value;
+  }
+
+  public String getPeriod() {
+    return period;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setValue(double value) {
+    this.value = value;
+  }
+
+  public void setPeriod(String period) {
+    this.period = period;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+}
