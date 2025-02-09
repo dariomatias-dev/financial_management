04eb082bd34e6e4a366ef8131c06ec84bb6089cf
dariomatias-dev <matiasdario75@gmail.com>
2025-02-09 15:53:33 -0300

    Criado tela principal e adicionado chamada para arquivo principal, criado tela de despesas e tela de criação de despesa.

--- src/main/java/org/cafe/Main.java
+++ src/main/java/org/cafe/Main.java
@@ -1,16 +1,11 @@
 package org.cafe;
 
+import org.cafe.views.MainView;
+
 public class Main {
   public static void main(String[] args) {
-    new Init();
-  }
-}
-
-class Init {
-  Init() {
-    run();
-  }
-
-  void run() {
+      MainView mainView =  new MainView();
+      
+      mainView.setVisible(true);
   }
 }
