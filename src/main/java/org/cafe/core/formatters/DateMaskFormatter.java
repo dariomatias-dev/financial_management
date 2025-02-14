e7a69b0bf9ef4c4e3e640e83921c301db0ccc9a7
dariomatias-dev <matiasdario75@gmail.com>
2025-02-14 19:31:10 -0300

    Criado classe de mascara de formatação de data.

---
+++ src/main/java/org/cafe/core/formatters/DateMaskFormatter.java
@@ -0,0 +1,107 @@
+package org.cafe.core.formatters;
+
+import java.text.ParseException;
+import javax.swing.*;
+import javax.swing.text.AbstractDocument;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DocumentFilter;
+import javax.swing.text.MaskFormatter;
+
+/**
+ * Classe de máscara de data "dd/MM/yyyy" para aplicação a um campo de texto
+ * (JFormattedTextField).
+ */
+public class DateMaskFormatter {
+  private MaskFormatter maskFormatter;
+
+  /**
+   * Construtor.
+   */
+  public DateMaskFormatter() {
+    try {
+      maskFormatter = new MaskFormatter("##/##/####");
+      maskFormatter.setPlaceholderCharacter('_');
+    } catch (ParseException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Aplica a máscara de data ao campo de texto.
+   *
+   * @param field Campo de texto ao qual a máscara será aplicada.
+   */
+  public void applyMask(JFormattedTextField field) {
+    if (maskFormatter != null) {
+      try {
+        maskFormatter.install(field);
+      } catch (Exception e) {
+      }
+
+      AbstractDocument doc = (AbstractDocument) field.getDocument();
+      doc.setDocumentFilter(new DateDocumentFilter());
+    }
+  }
+
+  /**
+   * Filtro para controlar a inserção, substituição e remoção de texto.
+   */
+  private class DateDocumentFilter extends DocumentFilter {
+    @Override
+    public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException {
+      if (canInsert(fb)) {
+        super.insertString(fb, offset, string, attr);
+        formatText(fb);
+      }
+    }
+
+    @Override
+    public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
+      if (canInsert(fb)) {
+        super.replace(fb, offset, length, text, attrs);
+        formatText(fb);
+      }
+    }
+
+    @Override
+    public void remove(FilterBypass fb, int offset, int length) throws BadLocationException {
+      super.remove(fb, offset, length);
+      fb.replace(0, fb.getDocument().getLength(), "__/__/____", null);
+    }
+
+    private boolean canInsert(FilterBypass fb) throws BadLocationException {
+      String text = fb.getDocument().getText(0, fb.getDocument().getLength());
+      return text.contains("_");
+    }
+
+    private void formatText(FilterBypass fb) throws BadLocationException {
+      String text = fb.getDocument().getText(0, fb.getDocument().getLength());
+      String formatted = formatDate(text);
+      fb.replace(0, fb.getDocument().getLength(), formatted, null);
+    }
+
+    private String formatDate(String text) {
+      StringBuilder formatted = new StringBuilder();
+      for (int i = 0; i < text.length(); i++) {
+        char c = text.charAt(i);
+        if (Character.isDigit(c)) {
+          formatted.append(c);
+        }
+      }
+
+      while (formatted.length() < 8) {
+        formatted.append('_');
+      }
+
+      if (formatted.length() >= 2) {
+        formatted.insert(2, '/');
+      }
+      if (formatted.length() >= 5) {
+        formatted.insert(5, '/');
+      }
+
+      return formatted.toString();
+    }
+  }
+}
