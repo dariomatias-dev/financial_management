0596f58f85fef7ecde218cd53d70809d105c2db0
dariomatias-dev <matiasdario75@gmail.com>
2025-02-16 11:43:32 -0300

    Criado classe para calcular valor do orçamento.

---
+++ src/main/java/org/cafe/utils/BudgetCalculator.java
@@ -0,0 +1,52 @@
+package org.cafe.utils;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import org.cafe.database.controllers.BudgetController;
+import org.cafe.models.budget.BudgetModel;
+import org.cafe.models.budget_item.BudgetItemModel;
+
+public class BudgetCalculator {
+  public BudgetModel calculate(
+          BudgetModel budget,
+          BudgetController budgetController,
+          List<BudgetItemModel> budgetItems
+  ) {
+    double totalBudget = 0.0;
+
+    for (BudgetItemModel item : budgetItems) {
+      long numberOfPeriods = calculatePeriods(item.getPeriod(), budget.getInitialDate().toLocalDate(), budget.getEndDate().toLocalDate());
+      double itemTotal = numberOfPeriods * item.getValue();
+      totalBudget += itemTotal;
+    }
+    
+    BudgetModel newBudget = new BudgetModel(
+                    budget.getId(),
+                    budget.getName(),
+                    budget.getDescription(),
+                    budget.getCategory(),
+                    budget.getStatus(),
+                    totalBudget,
+                    budget.getInitialDate(),
+                    budget.getEndDate()
+            );
+
+    budgetController.update(newBudget);
+    
+    return newBudget;
+  }
+
+  private long calculatePeriods(String period, LocalDate startDate, LocalDate endDate) {
+    return switch (period.toLowerCase()) {
+      case "diário" ->
+        ChronoUnit.DAYS.between(startDate, endDate);
+      case "semanal" ->
+        ChronoUnit.WEEKS.between(startDate, endDate);
+      case "mensal" ->
+        ChronoUnit.MONTHS.between(startDate, endDate);
+      default ->
+        throw new IllegalArgumentException("Invalid period type. Use 'diário', 'semanal', or 'mensal'.");
+    };
+  }
+}
