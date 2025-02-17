2e2d181761e687a27d537e9377abbb277a4b0f64
dariomatias-dev <matiasdario75@gmail.com>
2025-02-17 12:46:05 -0300

    Adicionado acesso provisório as telas de orçamento.

--- src/main/java/org/cafe/Main.java
+++ src/main/java/org/cafe/Main.java
@@ -1,16 +1,20 @@
 package org.cafe;
 
 import org.cafe.database.DatabaseService;
+import org.cafe.database.controllers.BudgetController;
+import org.cafe.database.controllers.BudgetItemController;
 import org.cafe.database.controllers.ExpenseController;
 import org.cafe.views.main.MainView;
 
 public class Main {
   public static void main(String[] args) {
-      DatabaseService databaseService = new DatabaseService();
-      ExpenseController expenseController = new ExpenseController(databaseService);
-      
-      MainView mainView =  new MainView(expenseController);
-      
-      mainView.setVisible(true);
+    DatabaseService databaseService = new DatabaseService();
+    ExpenseController expenseController = new ExpenseController(databaseService);
+    BudgetController budgetController = new BudgetController(databaseService);
+    BudgetItemController budgetItemController = new BudgetItemController(databaseService);
+
+    MainView mainView = new MainView(expenseController, budgetController, budgetItemController);
+
+    mainView.setVisible(true);
   }
 }
